01f6c82c0b527abc05e34b4cf8234dbde49e1eac
leeysi <yeseul726@e-mirim.hs.kr>
2017-06-01 20:20:21 +0900

    #5 Button Event Programming

--- app/src/main/java/yeseul/kr/hs/emirim/simplediary/MainActivity.java
+++ app/src/main/java/yeseul/kr/hs/emirim/simplediary/MainActivity.java
@@ -32,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 try{
-                    FileOutputStream fOut = openFileOutput(fileName, Context.MODE_PRIVATE));
+                    FileOutputStream fOut = openFileOutput(fileName, Context.MODE_PRIVATE);
                     String str = edit.getText().toString();
                     fOut.write(str.getBytes());
                     Toast.makeText(MainActivity.this, "정상적으로 " + fileName + "파일이 저장되었습니다.", Toast.LENGTH_LONG).show();
@@ -66,6 +66,7 @@ public class MainActivity extends AppCompatActivity {
             fIn.read(buf); //IOEception 예외처리가 필요함
             diaryStr = new String(buf).trim(); //읽어온 Byte값을 String으로 변환하는 방법, trim() 메소드를 사용하면 500Byte보다 적게 입력했을 시 남는 뒷부분의 공백들을 없애줌
             but.setText("수정 하기");
+            fIn.close();
         } catch (FileNotFoundException e) {
             edit.setText("일기가 존재하지 않습니다.");
             but.setText("새로 저장"); //버튼의 라벨을 "새로 저장"으로 설정
